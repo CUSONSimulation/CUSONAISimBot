3d975a5d5820b1957af5a6c96f5eed1c6dfccacc
chigkim <chigkim@users.noreply.github.com>
2025-01-17 21:36:55 -0500

    Black format

--- AISimBot.py
+++ AISimBot.py
@@ -13,11 +13,13 @@ import warnings
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+
 def load_settings():
     return tomllib.load(open("settings.toml", "rb"))
 
+
 def get_assistant(client):
-    name = st.session_state.settings['title']
+    name = st.session_state.settings["title"]
     print("Getting assistant:", name)
     has_more = True
     after = None
@@ -29,52 +31,50 @@ def get_assistant(client):
         has_more = assistants.has_more
         after = assistants.last_id
 
-        #Can't find, create a new one
-    assistant = client.beta.assistants.create(
-        name=name,
-        model="gpt-4o"
-    )
+        # Can't find, create a new one
+    assistant = client.beta.assistants.create(name=name, model="gpt-4o")
     return assistant.id
+
 
 def get_instruction():
     user_instruction = []
-    for key,val in st.session_state.settings["instruction"]["user"].items():
+    for key, val in st.session_state.settings["instruction"]["user"].items():
         if re.search(r"(jpg|png|webp)$", val):
             continue
         user_instruction.append(f"{key.replace("_", " ")}: {val}")
 
     user_instruction = "\n".join(user_instruction)
     assistant_instruction = []
-    for key,val in st.session_state.settings["instruction"]["assistant"].items():
+    for key, val in st.session_state.settings["instruction"]["assistant"].items():
         assistant_instruction.append(f"{key.replace("_", " ")}: {val}")
     assistant_instruction = "\n".join(assistant_instruction)
-    assistant_instruction = assistant_instruction.replace("{user_instruction}", user_instruction)
+    assistant_instruction = assistant_instruction.replace(
+        "{user_instruction}", user_instruction
+    )
     return assistant_instruction
+
 
 @st.cache_data
 def local_css(file_name):
     with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
 
 def speech_to_text(client, audio_data):
     with open(audio_data, "rb") as audio_file:
         transcript = client.audio.transcriptions.create(
-            model="whisper-1",
-            response_format="text",
-            file=audio_file
+            model="whisper-1", response_format="text", file=audio_file
         )
     return transcript
 
+
 def text_to_speech(client, input_text):
-    response = client.audio.speech.create(
-        model="tts-1",
-        voice="nova",
-        input=input_text
-    )
+    response = client.audio.speech.create(model="tts-1", voice="nova", input=input_text)
     webm_file_path = "temp_audio_play.mp3"
     with open(webm_file_path, "wb") as f:
         response.stream_to_file(webm_file_path)
     return webm_file_path
+
 
 def autoplay_audio(file_path: str):
     with open(file_path, "rb") as f:
@@ -87,6 +87,7 @@ def autoplay_audio(file_path: str):
     """
     st.markdown(md, unsafe_allow_html=True)
 
+
 # Function to generate Word document from the transcript
 def create_transcript_word():
     doc = Document()
@@ -95,11 +96,11 @@ def create_transcript_word():
     for message in st.session_state.messages:
         if message["role"] == "user":
             p = doc.add_paragraph()
-            p.add_run(st.session_state.settings['user_name'] + ": ").bold = True
+            p.add_run(st.session_state.settings["user_name"] + ": ").bold = True
             p.add_run(message["content"])
         else:
             p = doc.add_paragraph()
-            p.add_run(st.session_state.settings['assistant_name']+": ").bold = True
+            p.add_run(st.session_state.settings["assistant_name"] + ": ").bold = True
             p.add_run(message["content"])
 
     buffer = BytesIO()
@@ -107,50 +108,49 @@ def create_transcript_word():
     buffer.seek(0)
     return buffer
 
+
 def main():
     # Inject CSS for custom styles
     local_css("style.css")
 
-    client = OpenAI(api_key=st.secrets['OPENAI_API_KEY'])
-    if 'settings' not in st.session_state:
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+    if "settings" not in st.session_state:
         st.session_state.settings = load_settings()
-    if 'assistant_id' not in st.session_state:
+    if "assistant_id" not in st.session_state:
         st.session_state.assistant_id = get_assistant(client)
-    if 'assistant_instruction' not in st.session_state:
+    if "assistant_instruction" not in st.session_state:
         st.session_state.assistant_instruction = get_instruction()
     if "messages" not in st.session_state:
         st.session_state.messages = []
     if "manual_input" not in st.session_state:
         st.session_state.manual_input = None  # Add this line to initialize manual input
     if "end_session_button_clicked" not in st.session_state:
-        st.session_state.end_session_button_clicked = False  # Initialize the state to track the button click
-    if 'processed_audio' not in st.session_state:
+        st.session_state.end_session_button_clicked = (
+            False  # Initialize the state to track the button click
+        )
+    if "processed_audio" not in st.session_state:
         st.session_state.processed_audio = None  # Initialize the processed audio state
 
     st.title(st.session_state.settings["title"])
 
-
     st.sidebar.header("Chat with your Patient")
     container1 = st.sidebar.container(border=True)
     with container1:
-        for key,val in st.session_state.settings["instruction"]["user"].items():
+        for key, val in st.session_state.settings["instruction"]["user"].items():
             if re.search(r"(jpg|png|webp)$", val):
                 st.image(val, caption=key.replace("_", " "))
             else:
                 st.subheader(f"{key.replace("_", " ")}: {val}")
 
-
-
-
-
-
     # Button to start the chat session
     col1, col2, col3 = st.sidebar.columns([1, 1, 1])
     with col2:
-        if st.button('Start Chat'):
+        if st.button("Start Chat"):
             st.session_state["chat_active"] = True
             st.session_state.messages = []
-            st.session_state.show_text = False  # Hide the text when Start Chat is pressed
+            st.session_state.show_text = (
+                False  # Hide the text when Start Chat is pressed
+            )
 
     # Display text before Start Chat is pressed
     if "show_text" not in st.session_state:
@@ -159,18 +159,24 @@ def main():
     if st.session_state.show_text:
         container2 = st.container(border=True)
         with container2:
-            st.markdown(st.session_state.settings['intro'])
+            st.markdown(st.session_state.settings["intro"])
 
     # Check if chat is active
     if st.session_state.get("chat_active"):
-        st.sidebar.header('Speech Input')
+        st.sidebar.header("Speech Input")
 
         for message in st.session_state.messages:
-            if message["role"] == 'user':
-                with st.chat_message(st.session_state.settings['user_name'], avatar=st.session_state.settings['user_avatar']):
+            if message["role"] == "user":
+                with st.chat_message(
+                    st.session_state.settings["user_name"],
+                    avatar=st.session_state.settings["user_avatar"],
+                ):
                     st.markdown(message["content"])
             else:
-                with st.chat_message(st.session_state.settings['assistant_name'], avatar=st.session_state.settings['assistant_avatar']):
+                with st.chat_message(
+                    st.session_state.settings["assistant_name"],
+                    avatar=st.session_state.settings["assistant_avatar"],
+                ):
                     st.markdown(message["content"])
 
         # Check if there's a manual input and process it
@@ -181,7 +187,9 @@ def main():
             if st.session_state.end_session_button_clicked:
                 user_query = None  # Prevent any input after the end session
             else:
-                user_query = st.chat_input("Click 'End Session' Button to Receive Feedback and Download Transcript.")
+                user_query = st.chat_input(
+                    "Click 'End Session' Button to Receive Feedback and Download Transcript."
+                )
 
         # Create footer container for the microphone
         footer_container = st.sidebar.container(border=True)
@@ -192,7 +200,12 @@ def main():
             # st.write(audio_bytes)
 
             # Check if there is a new audio recording and it has not been processed yet
-            if audio_bytes and audio_bytes != st.session_state.processed_audio and audio_bytes != b'RIFF,\x00\x00\x00WAVEfmt \x10\x00\x00\x00\x01\x00\x02\x00\x80\xbb\x00\x00\x00\xee\x02\x00\x04\x00\x10\x00data\x00\x00\x00\x00':
+            if (
+                audio_bytes
+                and audio_bytes != st.session_state.processed_audio
+                and audio_bytes
+                != b"RIFF,\x00\x00\x00WAVEfmt \x10\x00\x00\x00\x01\x00\x02\x00\x80\xbb\x00\x00\x00\xee\x02\x00\x04\x00\x10\x00data\x00\x00\x00\x00"
+            ):
                 webm_file_path = "temp_audio.mp3"
                 with open(webm_file_path, "wb") as f:
                     f.write(audio_bytes)
@@ -200,14 +213,19 @@ def main():
                 if transcript:
                     os.remove(webm_file_path)
                     user_query = transcript  # Ensure you extract the text from the transcript object correctly
-                    st.session_state.processed_audio = audio_bytes  # Update the processed audio state
+                    st.session_state.processed_audio = (
+                        audio_bytes  # Update the processed audio state
+                    )
 
-            if audio_bytes == b'RIFF,\x00\x00\x00WAVEfmt \x10\x00\x00\x00\x01\x00\x02\x00\x80\xbb\x00\x00\x00\xee\x02\x00\x04\x00\x10\x00data\x00\x00\x00\x00':
-                st.error('Something went wrong. Kindly refresh the page and try again.')
+            if (
+                audio_bytes
+                == b"RIFF,\x00\x00\x00WAVEfmt \x10\x00\x00\x00\x01\x00\x02\x00\x80\xbb\x00\x00\x00\xee\x02\x00\x04\x00\x10\x00data\x00\x00\x00\x00"
+            ):
+                st.error("Something went wrong. Kindly refresh the page and try again.")
                 st.stop()
 
         if user_query:
-            if user_query.lower() == 'exit':
+            if user_query.lower() == "exit":
                 st.session_state["chat_active"] = False
                 st.stop()
 
@@ -225,9 +243,7 @@ def main():
 
             # Add user query to the thread
             client.beta.threads.messages.create(
-                thread_id=st.session_state.thread_id,
-                role="user",
-                content=user_query
+                thread_id=st.session_state.thread_id, role="user", content=user_query
             )
 
             # Stream the assistant's reply
@@ -257,7 +273,7 @@ def main():
                             assistant_reply_box.markdown(assistant_reply)
                     if event.data.object == "thread.message.delta":
                         for content in event.data.delta.content:
-                            if content.type == 'image_file':
+                            if content.type == "image_file":
                                 file_id = content.image_file.file_id
                                 image_data = client.files.content(file_id)
                                 image_data_bytes = image_data.read()
@@ -266,14 +282,14 @@ def main():
                                 st.image("my-image.png")
 
                 # Once the stream is over, update chat history
-                st.session_state.messages.append({"role": "assistant", "content": assistant_reply})
+                st.session_state.messages.append(
+                    {"role": "assistant", "content": assistant_reply}
+                )
                 if not st.session_state.end_session_button_clicked:
                     audio_file = text_to_speech(client, assistant_reply)
                     autoplay_audio(audio_file)
 
-
-
-    st.sidebar.warning(st.session_state.settings['warning'])
+    st.sidebar.warning(st.session_state.settings["warning"])
 
     # Button to show the download button
     if st.session_state.get("chat_active"):
@@ -284,7 +300,9 @@ def main():
                 if st.button("End Session"):
                     st.session_state.end_session_button_clicked = True
                     st.session_state["end_session_button"] = True
-                    st.session_state.manual_input = "Goodbye. Thank you for coming."  # Set manual input
+                    st.session_state.manual_input = (
+                        "Goodbye. Thank you for coming."  # Set manual input
+                    )
                     # Trigger the manual input immediately
                     st.rerun()
 
@@ -299,8 +317,9 @@ def main():
                 label="Download Transcript",
                 data=word_buffer,
                 file_name="Nurse_Patient_Full_Transcript.docx",
-                mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+                mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
             )
+
 
 if __name__ == "__main__":
     main()
